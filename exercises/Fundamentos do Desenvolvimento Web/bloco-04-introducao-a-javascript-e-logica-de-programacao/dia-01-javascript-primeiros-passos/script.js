83b78874b972f73464f1c7109d917c67655c273f
Robert Nascimento <robert1a7x@gmail.com>
2021-06-23 18:22:47 -0300

    Finalizado exercicio 1

---
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,34 @@
+// EXERCÍCIO 1
+
+// EXERCÍCIO 2
+let a = 10;
+let b = 5;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log("resultado adição = " + adicao + "\n" + 
+            "resultado subtração = " + subtracao + "\n" +
+            "resultado multiplicação = " + multiplicacao + "\n" +
+            "resultado divisão = " + divisao + "\n" +
+            "resultado modulo = " + modulo + "\n");
+// EXERCÍCIO 3
+
+// EXERCÍCIO 4
+
+// EXERCÍCIO 5
+
+// EXERCÍCIO 6
+
+// EXERCÍCIO 7
+
+// EXERCÍCIO 8
+
+// EXERCÍCIO 9
+
+// EXERCÍCIO 10
+
+// EXERCÍCIO 11
