3f440e4b993cea789e50ab1fb7f23fd00a135e37
Robert Nascimento <robert1a7x@gmail.com>
2021-06-23 18:38:16 -0300

    Finalizado exercicio 3

--- exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -22,10 +22,22 @@ if (n1 > n2) {
 
 } else {
   console.log("n2 é o maior número!");
-
 }
 
 // EXERCÍCIO 3
+let num1 = 8;
+let num2 = 9;
+let num3 = 7;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("num1 é o maior número!");
+
+} else if (num2 > num1 && num2 > num3) {
+    console.log("num2 é o maior número!");
+
+} else {
+    console.log("num3 é o maior número!");
+}
 
 // EXERCÍCIO 4
 
