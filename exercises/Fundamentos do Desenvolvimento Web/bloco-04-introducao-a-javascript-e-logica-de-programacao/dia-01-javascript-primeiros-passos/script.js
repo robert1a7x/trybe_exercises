bb561396f0f93bdf3e783ab64b59607713e7e991
Robert Nascimento <robert1a7x@gmail.com>
2021-06-23 19:14:53 -0300

    Finalizado exercicio 7

--- exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -96,6 +96,29 @@ switch (peca) {
 }
 
 // EXERCÍCIO 7
+let nota = 67;
+
+if (nota >= 90 && nota <= 100) {
+  console.log("Nota A");
+
+} else if (nota >= 80 && nota < 90) {
+    console.log("Nota B");
+
+} else if (nota >= 70 && nota < 80) {
+    console.log("Nota C");
+
+} else if (nota >= 60 && nota < 70) {
+    console.log("Nota D");
+
+} else if (nota >= 50 && nota < 60) {
+    console.log("Nota E");
+
+} else if (nota < 50 && nota >= 0) {
+    console.log("Nota F");
+
+} else {
+  console.log("Nota invalida, insira um valor válido")
+}
 
 // EXERCÍCIO 8
 
