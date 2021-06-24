b3114afccb0aa983b62f1d275599bed1c31de5cb
Robert Nascimento <robert1a7x@gmail.com>
2021-06-24 18:32:32 -0300

    Finalizado o exercicio 3

--- exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -2,19 +2,29 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //EXERCICIO 1
-  for (let i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
-  }
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+}
 
 //EXERCICIO 2
-  let soma = 0;
+let soma = 0;
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    soma += numbers[i];
-  }
+for (let i = 0; i < numbers.length; i += 1) {
+  soma += numbers[i];
+}
 
-  console.log("O resultado da soma dos números é: " + soma);
+console.log("O resultado da soma dos números é: " + soma);
+
 //EXERCICIO 3
+let somaM = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  somaM += numbers[i];
+}
+
+let media = somaM / numbers.length;
+
+console.log("A média de " + somaM + " é igual a = " + media);
 
 //EXERCICIO 4
 
