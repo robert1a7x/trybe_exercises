dd57e3f56c587a76b222412f6ab42ea61b781bc6
Robert Nascimento <robert1a7x@gmail.com>
2021-06-25 19:02:07 -0300

    Adicionado exercicio 3

--- exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritimos/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritimos/script.js
@@ -1,9 +1,9 @@
 //EXERCICIO 1
 let n1 = 6;
-let campo = "";
+let campo1 = "";
 
 for (let i = 0; i < n1; i += 1) {
-  campo += "*";
+  campo1 += "*";
 }
 
 for (let i = 0; i < n1; i += 1) {
@@ -12,11 +12,29 @@ for (let i = 0; i < n1; i += 1) {
 
 //EXERCICIO 2
 let n2 = 5;
-let vazio = "";
+let campo2= "";
 
 for (let i = 0; i < n2; i += 1) {
-  vazio += "*";
-  console.log(vazio);
+  campo2 += "*";
+  console.log(campo2);
 }
 
 //EXERCICIO 3
+let n3 = 5;
+let campo3= "";
+let posicao = n3;
+
+for (let i = 0; i < n3; i += 1) {
+  for (let h = 0; h <= n3; h += 1) {
+    if (h < posicao) {
+      campo3 = campo3 + " ";
+    } else {
+      campo3 =campo3 + "*";
+    }
+  }
+  console.log(campo3);
+  campo3 = "";
+  posicao -= 1;
+}
+
+//EXERCICIO 4
