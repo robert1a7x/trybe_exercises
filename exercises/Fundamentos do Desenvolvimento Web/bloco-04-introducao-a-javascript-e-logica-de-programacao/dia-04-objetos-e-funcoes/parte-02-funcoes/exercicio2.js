ed48a0a343a4460a05feac123b4185e70dc1ee5a
Robert Nascimento <robert1a7x@gmail.com>
2021-06-28 19:28:59 -0300

    Finalizado o exercicio 2 da parte 2

---
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/parte-02-funcoes/exercicio2.js
@@ -0,0 +1,15 @@
+let array = [2, 3, 6, 7, 10, 1];
+
+function maiorIndice(a) {
+  let maior = 0;
+  
+  for (let i in a) {
+    if (a[maior] < a[i]) {
+      maior = i;
+    }
+  }
+
+  return maior;
+}
+
+console.log(maiorIndice(array));
