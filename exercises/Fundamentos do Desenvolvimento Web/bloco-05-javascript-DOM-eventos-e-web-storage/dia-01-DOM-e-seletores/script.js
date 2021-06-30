74ca557df6a6232b5ff738661cf9bce4b7133bb3
Robert Nascimento <robert1a7x@gmail.com>
2021-06-30 18:39:09 -0300

    finalizado exercicio 3

--- exercises/Fundamentos do Desenvolvimento Web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-01-DOM-e-seletores/script.js
@@ -22,3 +22,9 @@ function mudarCorFundo() {
   document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
 }
 mudarCorFundo();
+
+//3
+function mudaCorCentro() {
+  document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+}
+mudaCorCentro();
