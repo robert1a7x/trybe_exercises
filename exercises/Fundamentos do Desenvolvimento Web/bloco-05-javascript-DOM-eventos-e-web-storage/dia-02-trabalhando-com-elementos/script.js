f26b8c7591d302bba7c410709273b8ac560573a9
Robert Nascimento <robert1a7x@gmail.com>
2021-07-05 15:59:02 -0300

    Finalizado o exercicio 3

--- exercises/Fundamentos do Desenvolvimento Web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -1,13 +1,18 @@
 let body = document.querySelector("body");
 
-//exercicio 1
+//exercicio 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body
 let title = document.createElement("h1");
 title.innerText = "Exercício 5.2 - JavaScript DOM";
 body.appendChild(title);
 
-//exercicio 2
+//exercicio 2 - Adicione a tag div com a classe main-content como filho da tag body
 let divMainContent = document.createElement("div");
 divMainContent.className = "main-content";
 body.appendChild(divMainContent);
 
-//exercicio 3
+//exercicio 3 - Adicione a tag div com a classe center-content como filho da tag div criada no passo 2
+let divCenterContent = document.createElement("div");
+divCenterContent.className = "center-content";
+divMainContent.appendChild(divCenterContent);
+
+//exercicio 4 - Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
