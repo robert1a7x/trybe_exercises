bd33b1e11ca6f257a6f58b02b9adf8fea4c758dc
Robert Nascimento <robert1a7x@gmail.com>
2021-07-05 15:49:06 -0300

    Finalizado o exercicio 1

---
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-05-javascript-DOM-eventos-e-web-storage/dia-02-trabalhando-com-elementos/script.js
@@ -0,0 +1,7 @@
+//exercicio 1
+let body = document.querySelector("body");
+let title = document.createElement("h1");
+title.innerText = "Exercício 5.2 - JavaScript DOM";
+body.appendChild(title);
+
+//exercicio 2
