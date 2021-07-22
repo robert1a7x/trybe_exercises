ee19d55ec2f7fb9467bda7c2ffc72f3ed378bced
Robert Nascimento <robert1a7x@gmail.com>
2021-07-22 18:37:08 -0300

    finalizado exercicio 2 da parte 1

--- exercises/Fundamentos do Desenvolvimento Web/bloco-07-indroducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-function-e-templete-literals/parte1/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-07-indroducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-function-e-templete-literals/parte1/script.js
@@ -21,6 +21,7 @@ testingScope(true);
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+oddsAndEvens.sort((a, b) => a - b);
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
