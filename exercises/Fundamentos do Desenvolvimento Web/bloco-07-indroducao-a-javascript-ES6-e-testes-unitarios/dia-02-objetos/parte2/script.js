ddbaf76426991142322ca05a9b4b3fa889f3a7ac
Robert Nascimento <robert1a7x@gmail.com>
2021-07-23 19:02:20 -0300

    Finalizado exercicio 3 parte 2

--- exercises/Fundamentos do Desenvolvimento Web/bloco-07-indroducao-a-javascript-ES6-e-testes-unitarios/dia-02-objetos/parte2/script.js
+++ exercises/Fundamentos do Desenvolvimento Web/bloco-07-indroducao-a-javascript-ES6-e-testes-unitarios/dia-02-objetos/parte2/script.js
@@ -23,12 +23,18 @@ const addLesson2 = (obj, key, value) => obj[key] = value;
 
 addLesson2(lesson2, 'turno', 'manhã');
 console.log(lesson2);
+console.log('-------------------------------------')
 
 //2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const listObjectKeys = obj => Object.keys(obj);
 
 console.log(listObjectKeys(lesson3));
+console.log('-------------------------------------')
 
 //3 - Crie uma função para mostrar o tamanho de um objeto.
+const ObjectSize = obj => Object.keys(obj).length;
+
+console.log(`O objeto lesson1 tem ${ObjectSize(lesson1)} linhas de tamanho`);
+console.log('-------------------------------------')
 
 //4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
