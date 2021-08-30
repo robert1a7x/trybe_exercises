d7062289de38b900456d18a30f1fc87983ec4e48
Robert Nascimento <robert1a7x@gmail.com>
2021-08-30 18:24:43 -0300

    Exercicio 1

--- exercises/02-Desenvolvimento Front-end/bloco-11-introducao-react/dia-01-hello-world-no-react/my-app/src/App.js
+++ exercises/02-Desenvolvimento Front-end/bloco-11-introducao-react/dia-01-hello-world-no-react/my-app/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 
+// 1 - Crie uma lista de tarefas, chame a função dentro do seu componente App. Insira o valor que você quiser, salve a página e inicie-a rodando o comando npm start
 const Task = (value) => {
   return (
-    <li>{value}</li>
+    <li key ={value}>{value}</li>
   );
 }
 
+// 2 - crie uma array de compromissos e use a função map para que cada item do array apareça, como um item de lista, no seu componente App 
+const array = ['Andar', 'Treinar Javascript', 'Pesquisar bicicleta', 'Jogar MHW'];
+
 function App() {
   return (
-    <ul>{Task('Aprender React')}</ul>
+    <ul>
+      {Task('Aprender React')}
+      {array.map((element) => Task(element))}
+    </ul>
   );
 }
 
